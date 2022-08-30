a300ea57ed6ae0f92d72e2c35ec266d2d7aaa5d3
Folarin Lawal <lawalfolarin6@gmail.com>
2022-08-30 21:37:48 +0000

    initialised project with tailwindcss and nextjs

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@700,400,600,300,500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=space-grotesk@300,700,500,400,600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
